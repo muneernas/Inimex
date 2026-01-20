9b756004215d8e31223b60a54a5725b678ff011b
muneer <muneer@simplify9.com>
2026-01-20 20:47:39 +0300

    ff

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import ContactUs from "./pages/ContactUs";
+
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Header />
-
-            <main className="min-h-screen">
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <div className="p-6">
-                                <h1 className="text-3xl font-bold">Welcome to Inimex</h1>
-                            </div>
-                        }
-                    />
-                    <Route path="/contact-us" element={<ContactUs />} />
-                </Routes>
-            </main>
-
-            <Footer />
-        </Router>
+        <div>
+            {/* Your app components go here */}
+            <p> hello</p>
+        </div>
     );
 };
 
